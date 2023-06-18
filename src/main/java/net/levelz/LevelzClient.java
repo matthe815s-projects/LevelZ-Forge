a43df0f909aba60ead2625b938de212e051e54c0
Matthe815 <matthew.elswick@matthe815.dev>
2023-06-18 16:20:37 -0400

    refactor: Rewrote level client

--- src/main/java/net/levelz/LevelzClient.java
+++ src/main/java/net/levelz/LevelzClient.java
@@ -1,17 +1,33 @@
 package net.levelz;
 
-import net.fabricmc.api.ClientModInitializer;
-import net.levelz.init.KeyInit;
-import net.levelz.init.RenderInit;
+import net.levelz.init.*;
 import net.levelz.network.PlayerStatsClientPacket;
+import net.levelz.network.PlayerStatsServerPacket;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 
-public class LevelzClient implements ClientModInitializer {
+@Mod(modid = LevelzClient.MODID)
+public class LevelzClient {
+    public static final String MODID = "LEVEL-Z";
 
-    @Override
-    public void onInitializeClient() {
+    @Mod.EventHandler
+    public void onInitialize(FMLInitializationEvent event) {
+        System.out.println("Initialized");
+
         KeyInit.init();
         PlayerStatsClientPacket.init();
         RenderInit.init();
+
+        CommandInit.init();
+        CompatInit.init();
+        ConfigInit.init();
+        CriteriaInit.init();
+        EntityInit.init();
+        EventInit.init();
+        JsonReaderInit.init();
+        PlayerStatsServerPacket.init();
+        TagInit.init();
+        ItemInit.init();
     }
 
 }
