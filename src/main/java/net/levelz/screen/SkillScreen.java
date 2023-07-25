d3884577b8117606fc39397cf004b7dc81f723a0
Matthe815 <matthew.elswick@matthe815.dev>
2023-07-25 19:06:29 -0400

    refactor: SkillScreen refactored

--- src/main/java/net/levelz/screen/SkillScreen.java
+++ src/main/java/net/levelz/screen/SkillScreen.java
@@ -1,33 +1,37 @@
 package net.levelz.screen;
 
-import java.awt.*;
+import java.io.IOException;
 import java.math.BigDecimal;
 import java.math.RoundingMode;
 import java.util.ArrayList;
+import java.util.List;
 import java.util.Map;
-import java.util.Objects;
 
+import com.google.common.collect.Multimap;
 import net.minecraft.client.Minecraft;
+import net.minecraft.client.gui.GuiButton;
 import net.minecraft.client.gui.GuiScreen;
 import net.minecraft.client.gui.ScaledResolution;
 import net.minecraft.entity.SharedMonsterAttributes;
+import net.minecraft.entity.ai.attributes.AttributeModifier;
 import net.minecraft.entity.player.EntityPlayer;
-import net.minecraft.entity.player.InventoryPlayer;
-import net.minecraft.inventory.InventoryBasic;
+import net.minecraft.inventory.EntityEquipmentSlot;
+import net.minecraft.item.*;
 import net.minecraft.util.ResourceLocation;
 import net.minecraft.util.text.TextComponentTranslation;
+import net.minecraft.util.text.event.ClickEvent;
+import net.minecraftforge.fml.common.registry.ForgeRegistries;
 import net.minecraftforge.fml.relauncher.Side;
 import net.minecraftforge.fml.relauncher.SideOnly;
 
 import net.levelz.access.PlayerStatsManagerAccess;
 import net.levelz.data.LevelLists;
 import net.levelz.init.ConfigInit;
-import net.levelz.init.KeyInit;
 import net.levelz.network.PlayerStatsClientPacket;
 import net.levelz.stats.PlayerStatsManager;
 import net.levelz.stats.Skill;
-import net.minecraft.item.Item;
-import sun.security.provider.SHA;
+
+import javax.annotation.Nullable;
 
 @SideOnly(Side.CLIENT)
 public class SkillScreen extends GuiScreen {
@@ -71,31 +75,26 @@ public class SkillScreen extends GuiScreen {
                     this.skillButtons[i].addTooltip(tooltip);
                 }
             }
-            this.levelButtons[i] = this.addButton(new WidgetButtonPage(this.x + 83 + (i > 5 ? 90 : 0), this.y + 92 + i * 20 - (i > 5 ? 120 : 0), 13, 13, 33, 42, true, true, null, button -> {
+            this.levelButtons[i] = this.addButton(new WidgetButtonPage(this.x + 83 + (i > 5 ? 90 : 0), this.y + 92 + i * 20 - (i > 5 ? 120 : 0), 13, 13, 33, 42, true, true, null, (button) -> {
                 int level = 1;
-                if (((WidgetButtonPage) button).wasRightButtonClicked()) {
-                    level = 5;
-                } else if (((WidgetButtonPage) button).wasMiddleButtonClicked()) {
-                    level = 10;
-                }
                 PlayerStatsClientPacket.writeC2SIncreaseLevelPacket(this.playerStatsManager, Skill.values()[skillInt], level);
             }));
             //this.levelButtons[i].enabled = playerStatsManager.getSkillPoints() > 0 && this.playerStatsManager.getSkillLevel(Skill.values()[i]) < ConfigInit.CONFIG.maxLevel;
         }
 
-        WidgetButtonPage infoButton = this.addButton(new WidgetButtonPage(this.x + 178, this.y + 73, 11, 13, 0, 42, true, false, new TextComponentTranslation("text.levelz.more_info")));
+        WidgetButtonPage infoButton = this.addButton(new WidgetButtonPage(this.x + 178, this.y + 73, 11, 13, 0, 42, true, false, new TextComponentTranslation("text.levelz.more_info"), null));
         String[] infoTooltip = new TextComponentTranslation("text.levelz.gui.level_up_skill.tooltip").getFormattedText().split("\n");
         for (int i = 0; i < infoTooltip.length; i++) {
             infoButton.addTooltip(new TextComponentTranslation(infoTooltip[i]));
         }
 
         if (!LevelLists.craftingItemList.isEmpty()) {
-            this.addButton(new WidgetButtonPage(this.x + 180, this.y + 5, 15, 13, 0, 80, true, true, Text.translatable("text.levelz.crafting_info"), button -> {
+            this.addButton(new WidgetButtonPage(this.x + 180, this.y + 5, 15, 13, 0, 80, true, true, new TextComponentTranslation("text.levelz.crafting_info"), button -> {
                 Minecraft.getMinecraft().displayGuiScreen(new SkillListScreen("crafting"));
             }));
         }
         if (!ConfigInit.CONFIG.useIndependentExp) {
-            WidgetButtonPage levelUpButton = this.addButton(new WidgetButtonPage(this.x + 177, this.y + 49, 13, 13, 33, 42, true, true, Text.translatable("text.levelz.level_up"), button -> {
+            WidgetButtonPage levelUpButton = this.addButton(new WidgetButtonPage(this.x + 177, this.y + 49, 13, 13, 33, 42, true, true, new TextComponentTranslation("text.levelz.level_up"), button -> {
                 int level = 1;
                 if (((WidgetButtonPage) button).wasRightButtonClicked()) {
                     level = 5;
@@ -110,6 +109,11 @@ public class SkillScreen extends GuiScreen {
             }
             //levelUpButton.active = !playerStatsManager.isMaxLevel() && (this.playerStatsManager.getNonIndependentExperience() / this.playerStatsManager.getNextLevelExperience()) >= 1;
         }
+    }
+
+    @Override
+    public boolean doesGuiPauseGame() {
+        return false;
     }
 
     @Override
@@ -166,7 +170,7 @@ public class SkillScreen extends GuiScreen {
 
             boolean skillsAllMaxed = true;
             for (int o = 0; o < this.levelButtons.length; o++) {
-                this.levelButtons[o].active = playerStatsManager.getSkillPoints() > 0 && this.playerStatsManager.getSkillLevel(Skill.values()[o]) < ConfigInit.CONFIG.maxLevel;
+                this.levelButtons[o].enabled = playerStatsManager.getSkillPoints() > 0 && this.playerStatsManager.getSkillLevel(Skill.values()[o]) < ConfigInit.CONFIG.maxLevel;
                 if (skillsAllMaxed) {
                     skillsAllMaxed = this.playerStatsManager.getSkillLevel(Skill.values()[o]) >= ConfigInit.CONFIG.maxLevel;
                 }
@@ -179,38 +183,38 @@ public class SkillScreen extends GuiScreen {
 
             // Small icons text
             for (int o = 0; o < 12; o++) {
-                Text currentLevelText = Text.translatable("text.levelz.gui.current_level", playerStatsManager.getSkillLevel(Skill.values()[o]), ConfigInit.CONFIG.maxLevel);
-                context.drawText(this.textRenderer, currentLevelText, this.x - this.textRenderer.getWidth(currentLevelText) / 2 + 57 + (o > 5 ? 90 : 0), this.y + 95 + o * 20 - (o > 5 ? 120 : 0),
+                TextComponentTranslation currentLevelText = new TextComponentTranslation("text.levelz.gui.current_level", playerStatsManager.getSkillLevel(Skill.values()[o]), ConfigInit.CONFIG.maxLevel);
+                this.fontRenderer.drawString(currentLevelText.getFormattedText(), this.x - this.fontRenderer.getStringWidth(currentLevelText.getFormattedText()) / 2 + 57 + (o > 5 ? 90 : 0), this.y + 95 + o * 20 - (o > 5 ? 120 : 0),
                         0x3F3F3F, false);
             }
         }
         // Small icons
-        RenderSystem.setShaderTexture(0, ICON_TEXTURES);
-        context.drawTexture(ICON_TEXTURES, this.x + 58, this.y + 21, 0, 0, 9, 9); // lifeIcon
-        context.drawTexture(ICON_TEXTURES, this.x + 58, this.y + 34, 9, 0, 9, 9); // protectionIcon
-        context.drawTexture(ICON_TEXTURES, this.x + 108, this.y + 21, 18, 0, 9, 9); // speedIcon
-        context.drawTexture(ICON_TEXTURES, this.x + 108, this.y + 34, 27, 0, 9, 9); // damageIcon
-        context.drawTexture(ICON_TEXTURES, this.x + 155, this.y + 21, 36, 0, 9, 9); // foodIcon
-        context.drawTexture(ICON_TEXTURES, this.x + 155, this.y + 34, 45, 0, 9, 9); // fortuneIcon
+        Minecraft.getMinecraft().ingameGUI.drawTexturedModalRect(this.x + 58, this.y + 21, 0, 0, 9, 9); // lifeIcon
+        Minecraft.getMinecraft().ingameGUI.drawTexturedModalRect(this.x + 58, this.y + 34, 9, 0, 9, 9); // protectionIcon
+        Minecraft.getMinecraft().ingameGUI.drawTexturedModalRect(this.x + 108, this.y + 21, 18, 0, 9, 9); // speedIcon
+        Minecraft.getMinecraft().ingameGUI.drawTexturedModalRect(this.x + 108, this.y + 34, 27, 0, 9, 9); // damageIcon
+        Minecraft.getMinecraft().ingameGUI.drawTexturedModalRect(this.x + 155, this.y + 21, 36, 0, 9, 9); // foodIcon
+        Minecraft.getMinecraft().ingameGUI.drawTexturedModalRect(this.x + 155, this.y + 34, 45, 0, 9, 9); // fortuneIcon
 
-        DrawTabHelper.drawTab(client, context, this, x, y, mouseX, mouseY);
-        super.render(context, mouseX, mouseY, delta);
+        //DrawTabHelper.drawTab(client, context, this, x, y, mouseX, mouseY);
+        super.drawScreen(mouseX, mouseY, delta);
     }
 
     @Override
-    public boolean keyPressed(int keyCode, int scanCode, int modifiers) {
-        if (KeyInit.screenKey.matchesKey(keyCode, scanCode) || Objects.requireNonNull(client).options.inventoryKey.matchesKey(keyCode, scanCode)) {
-            this.close();
-            return true;
-        } else {
-            return super.keyPressed(keyCode, scanCode, modifiers);
-        }
+    public void handleKeyboardInput() throws IOException {
+//        if (KeyInit.screenKey.matchesKey(keyCode, scanCode) || Objects.requireNonNull(client).options.inventoryKey.matchesKey(keyCode, scanCode)) {
+//            this.close();
+//            return true;
+//        } else {
+//            return super.keyPressed(keyCode, scanCode, modifiers);
+//        }
+        super.handleKeyboardInput();
     }
 
     @Override
-    public boolean mouseClicked(double mouseX, double mouseY, int button) {
-        DrawTabHelper.onTabButtonClick(client, this, this.x, this.y, mouseX, mouseY, this.getFocused() != null);
-        return super.mouseClicked(mouseX, mouseY, button);
+    public void handleMouseInput() throws IOException {
+//        DrawTabHelper.onTabButtonClick(client, this, this.x, this.y, mouseX, mouseY, this.getFocused() != null);
+        super.handleMouseInput();
     }
 
     public int getWidth() {
@@ -223,63 +227,67 @@ public class SkillScreen extends GuiScreen {
 
     private String getDamageLabel() {
         float damage = 0.0F;
-        Item item = playerEntity.getMainHandStack().getItem();
+        Item item = playerEntity.getHeldItemMainhand().getItem();
         ArrayList<Object> levelList = LevelLists.customItemList;
-        if (!levelList.isEmpty() && PlayerStatsManager.playerLevelisHighEnough(playerEntity, levelList, Registries.ITEM.getId(item).toString(), false)) {
-            if (item instanceof SwordItem swordItem) {
-                damage = swordItem.getAttackDamage();
-            } else if (item instanceof MiningToolItem miningToolItem) {
-                damage = miningToolItem.getAttackDamage();
-            } else if (!item.getAttributeModifiers(EquipmentSlot.MAINHAND).isEmpty() && item.getAttributeModifiers(EquipmentSlot.MAINHAND).containsKey(EntityAttributes.GENERIC_ATTACK_DAMAGE)) {
-                Multimap<EntityAttribute, EntityAttributeModifier> multimap = item.getAttributeModifiers(EquipmentSlot.MAINHAND);
-                for (Map.Entry<EntityAttribute, EntityAttributeModifier> entry : multimap.entries()) {
-                    if (entry.getKey().equals(EntityAttributes.GENERIC_ATTACK_DAMAGE)) {
-                        damage = (float) entry.getValue().getValue();
+        if (!levelList.isEmpty() && PlayerStatsManager.playerLevelisHighEnough(playerEntity, levelList, ForgeRegistries.ITEMS.getKey(item).toString(), false)) {
+            if (item instanceof ItemSword) {
+                damage = ((ItemSword) item).getAttackDamage();
+            } else if (item instanceof ItemTool) {
+                damage = 1;
+            } else if (!item.getAttributeModifiers(EntityEquipmentSlot.MAINHAND, playerEntity.getHeldItemMainhand()).isEmpty() && item.getAttributeModifiers(EntityEquipmentSlot.MAINHAND, playerEntity.getHeldItemMainhand()).containsKey(SharedMonsterAttributes.ATTACK_DAMAGE)) {
+                Multimap<String, AttributeModifier> multimap = item.getAttributeModifiers(EntityEquipmentSlot.MAINHAND, playerEntity.getHeldItemMainhand());
+                for (Map.Entry<String, AttributeModifier> entry : multimap.entries()) {
+                    if (entry.getKey().equals(SharedMonsterAttributes.ATTACK_DAMAGE)) {
+                        damage = (float) entry.getValue().getAmount();
                         break;
                     }
                 }
             }
-        } else if (item instanceof ToolItem toolItem) {
+        } else if (item instanceof ItemTool) {
             levelList = null;
-            if (item instanceof SwordItem) {
+            if (item instanceof ItemSword) {
                 levelList = LevelLists.swordList;
-            } else if (item instanceof AxeItem) {
+            } else if (item instanceof ItemAxe) {
                 if (ConfigInit.CONFIG.bindAxeDamageToSwordRestriction) {
                     levelList = LevelLists.swordList;
                 } else {
                     levelList = LevelLists.axeList;
                 }
-            } else if (item instanceof HoeItem) {
+            } else if (item instanceof ItemHoe) {
                 levelList = LevelLists.hoeList;
-            } else if (item instanceof PickaxeItem || item instanceof ShovelItem) {
+            } else if (item instanceof ItemPickaxe || item instanceof ItemSpade) {
                 levelList = LevelLists.toolList;
             }
             if (levelList != null) {
-                if (PlayerStatsManager.playerLevelisHighEnough(playerEntity, levelList, toolItem.getMaterial().toString().toLowerCase(), false)) {
-                    if (item instanceof SwordItem swordItem) {
-                        damage = swordItem.getAttackDamage();
-                    } else if (item instanceof MiningToolItem miningToolItem) {
-                        damage = miningToolItem.getAttackDamage();
+                if (PlayerStatsManager.playerLevelisHighEnough(playerEntity, levelList, ((ItemTool) item).toString().toLowerCase(), false)) {
+                    if (item instanceof ItemSword) {
+                        damage = ((ItemSword) item).getAttackDamage();
+                    } else if (item instanceof ItemPickaxe) {
+                        damage = 2;
                     }
                 }
             }
         }
 
-        damage += playerEntity.getAttributeValue(EntityAttributes.GENERIC_ATTACK_DAMAGE);
+        damage += playerEntity.getEntityAttribute(SharedMonsterAttributes.ATTACK_DAMAGE).getAttributeValue();
         return String.valueOf(BigDecimal.valueOf(damage).setScale(2, RoundingMode.HALF_DOWN).floatValue());
     }
 
-    public static class WidgetButtonPage extends ButtonWidget {
+    public interface OnPress {
+        public void Action(GuiButton button);
+    }
+
+    public static class WidgetButtonPage extends GuiButton {
 
         private final boolean hoverOutline;
         private final boolean clickable;
         private final int textureX;
         private final int textureY;
-        private List<Text> tooltip = new ArrayList<Text>();
+        private List<TextComponentTranslation> tooltip = new ArrayList<>();
         private int clickedKey = -1;
 
-        public WidgetButtonPage(int x, int y, int sizeX, int sizeY, int textureX, int textureY, boolean hoverOutline, boolean clickable, @Nullable Text tooltip, ButtonWidget.PressAction onPress) {
-            super(x, y, sizeX, sizeY, ScreenTexts.EMPTY, onPress, DEFAULT_NARRATION_SUPPLIER);
+        public WidgetButtonPage(int x, int y, int sizeX, int sizeY, int textureX, int textureY, boolean hoverOutline, boolean clickable, @Nullable TextComponentTranslation tooltip, OnPress action) {
+            super(1, x, y, sizeX, sizeY, "");
             this.hoverOutline = hoverOutline;
             this.clickable = clickable;
             this.textureX = textureX;
@@ -292,42 +300,24 @@ public class SkillScreen extends GuiScreen {
         }
 
         @Override
-        public void renderButton(DrawContext context, int mouseX, int mouseY, float delta) {
-            MinecraftClient minecraftClient = MinecraftClient.getInstance();
-            context.setShaderColor(1.0f, 1.0f, 1.0f, this.alpha);
-            RenderSystem.enableBlend();
-            RenderSystem.enableDepthTest();
+        public void drawButton(Minecraft mc, int mouseX, int mouseY, float delta) {
+            Minecraft minecraftClient = Minecraft.getMinecraft();
             int i = hoverOutline ? this.getTextureY() : 0;
-            context.drawTexture(ICON_TEXTURES, this.getX(), this.getY(), this.textureX + i * this.width, this.textureY, this.width, this.height);
-            context.setShaderColor(1.0f, 1.0f, 1.0f, 1.0f);
-            if (this.isHovered()) {
-                context.drawTooltip(minecraftClient.textRenderer, this.tooltip, mouseX, mouseY);
+            //context.drawTexture(ICON_TEXTURES, this.getX(), this.getY(), this.textureX + i * this.width, this.textureY, this.width, this.height);
+            if (this.hovered) {
+                //context.drawTooltip(minecraftClient.textRenderer, this.tooltip, mouseX, mouseY);
             }
         }
 
         @Override
-        public boolean mouseClicked(double mouseX, double mouseY, int button) {
-            this.clickedKey = button;
+        public boolean mousePressed(Minecraft mc, int mouseX, int mouseY) {
             if (!this.clickable) {
                 return false;
             }
-            return super.mouseClicked(mouseX, mouseY, button);
+            return super.mousePressed(mc, mouseX, mouseY);
         }
 
-        @Override
-        protected boolean isValidClickButton(int button) {
-            return super.isValidClickButton(button) || button == 1 || button == 2;
-        }
-
-        @Override
-        public boolean keyPressed(int keyCode, int scanCode, int modifiers) {
-            if (!this.clickable) {
-                return false;
-            }
-            return super.keyPressed(keyCode, scanCode, modifiers);
-        }
-
-        public void addTooltip(Text text) {
+        public void addTooltip(TextComponentTranslation text) {
             this.tooltip.add(text);
         }
 
@@ -341,10 +331,8 @@ public class SkillScreen extends GuiScreen {
 
         private int getTextureY() {
             int i = 1;
-            if (!this.active) {
+            if (!this.enabled) {
                 i = 0;
-            } else if (this.isSelected()) {
-                i = 2;
             }
             return i;
         }
